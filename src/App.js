825fb530c771cb04eacf3fae12947c01dc3c8d2a
marior9 <mariorodriguezan@gmail.com>
2020-09-14 12:51:52 -0400

    Bootstrap UI added and Gridbounds input added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import { InputGroup, FormControl } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Mars Rover</h1>
-    </div>
-  );
+
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {gridX: '',gridY: '' };
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="input-location">
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1">Grid Bounds </InputGroup.Text>
+          <FormControl className="input-group" onChange={(e)=>{this.setState({gridX: e.currentTarget.value})}}
+          aria-label="gridPositionX"
+          />
+          <FormControl className="input-group" onChange={(e)=>{this.setState({gridY: e.currentTarget.value})}}
+          aria-label="gridPositionY"
+          />
+          </InputGroup.Prepend>
+        </InputGroup>
+        </div>
+   
+      </div>
+    );
+  }
+  
 }
 
-export default App;
