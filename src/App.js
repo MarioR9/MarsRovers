7e1d9f73f7601f78cddcb47cecb9fd0094f6281c
marior9 <mariorodriguezan@gmail.com>
2020-09-14 15:10:36 -0400

    #of rovers input fild for inital location and movements added acording to the amount of rovers being deployed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
-import { InputGroup, FormControl } from 'react-bootstrap';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 
+let counter = 1;
 export default class App extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {gridX: '',gridY: '', numberOfRovers:'',roverMoves:'' };
+    this.state = {gridX: '',gridY: '', numberOfRovers: 0 ,roverMoves: '', deployedRovers: []};
   }
-  handleMovements=(e)=>{
+  
+  handleMovements=(e)=>{ //Regex will prevent from storing any numbers or symbols. Only storeing set moves either M or L.
     const checkForLorM = /[^l^m]/gi;
     let moves = e.currentTarget.value.replace(checkForLorM,'')
     this.setState({roverMoves: moves.toUpperCase()})
+  }
+  handleRovers=()=>{ //handler will create instaces of the desired amount of rovers and push them into and array.
+   let rovers = []
+   let nOfRovers = parseInt(this.state.numberOfRovers)
+   for(let i=0; i < nOfRovers; i++){
+     rovers.push(`rover#${i}`)
+   }
+   this.setState({deployedRovers: rovers})
+   console.log(this.state.deployedRovers)
   }
   render(){
     return (
@@ -35,24 +46,28 @@ export default class App extends React.Component{
           <FormControl className="input-group" onChange={(e)=>{this.setState({numberOfRovers: e.currentTarget.value})}}
           aria-label="numberOfRovers"
           />
+          <Button onClick={this.handleRovers} variant="info">Generate</Button>{' '}
           </InputGroup.Prepend>
         </InputGroup>
         </div>
-        <div>
-        <InputGroup className="mb-3"> 
-          <InputGroup.Prepend>
-          <InputGroup.Text id="basic-addon1">Rover#{this.state.numberOfRovers} location</InputGroup.Text>
-          <FormControl className="input-group" 
-          aria-label="gridPositionX"
-          />
-          <InputGroup.Text id="basic-addon1">Movments</InputGroup.Text>
-          <FormControl className="input-group-move" onChange={this.handleMovements}
-          aria-label="numberOfRovers"
-          />
-          </InputGroup.Prepend>
-        </InputGroup>
-        </div>
+        {this.state.deployedRovers.map(rover => 
         
+          <div > 
+          <InputGroup className="mb-3"> 
+            <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">Rover#{counter++} init location</InputGroup.Text>
+            <FormControl className="input-group" 
+            aria-label="gridPositionX"
+            />
+            <InputGroup.Text id="basic-addon1">Movments</InputGroup.Text>
+            <FormControl className="input-group-move" onChange={this.handleMovements}
+            aria-label="numberOfRovers"
+            />
+            </InputGroup.Prepend>
+          </InputGroup>
+          </div>
+
+          )}
 
         </div>
    
