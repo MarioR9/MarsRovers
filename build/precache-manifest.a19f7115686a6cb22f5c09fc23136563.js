3104c523b3a29de9caf1f72edb88c4a99aef257d
marior9 <mariorodriguezan@gmail.com>
2020-09-15 15:32:09 -0400

    minor fixes

---
+++ build/precache-manifest.a19f7115686a6cb22f5c09fc23136563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4b0669d1a67d9ec366d9059659a4ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c79f4561d10efb7522b9",
+    "url": "/static/css/main.2e2b5dc4.chunk.css"
+  },
+  {
+    "revision": "e8f01c9aa4312c34be02",
+    "url": "/static/js/2.bf30b364.chunk.js"
+  },
+  {
+    "revision": "4a698b4b6a4fdc5cd15b0527fa0f5b18",
+    "url": "/static/js/2.bf30b364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79f4561d10efb7522b9",
+    "url": "/static/js/main.bcb685d9.chunk.js"
+  },
+  {
+    "revision": "df6136509b9d750dd8d0",
+    "url": "/static/js/runtime-main.272b8a55.js"
+  }
+]);
